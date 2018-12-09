8931fe792bcee03709eb1221fb691e80e5456980
Junji Shiokawa <junji.shiokawa@fork.co.jp>
2018-12-09 21:01:34 +0900

    jp config

--- app/js/app.js
+++ app/js/app.js
@@ -37,8 +37,10 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 /* jshint ignore:end */
 
+/* original:
 ga('create', 'UA-39334307-1', 'auto', {'siteSpeedSampleRate': 50});
 ga('create', 'UA-49880327-9', 'auto', {'name': 'devrelTracker'});
 recordPageview();
+*/
 
 })(window);
